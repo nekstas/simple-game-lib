def5f21651882012e750ca388de137714d374664
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2025-12-03 22:53:10 +0300

    Add printing of result url in build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ cd build
 flet publish main.py --distpath "../r/$2-$hex" --base-url "r/$2-$hex"
 git add "../r/$2-$hex"
 git commit -m "Add result files for $2-$hex."
+echo "https://s.nekstas.ru/r/$2-$hex"
