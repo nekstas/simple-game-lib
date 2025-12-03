fda74dee6f77325596afab9342275dae6a6437c1
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2025-12-04 01:26:07 +0300

    Add result files for step01-8d6a.

---
+++ extra/step01.py
@@ -0,0 +1,20 @@
+from lib import *
+
+
+def init(state, is_first_time):
+    pass
+
+
+def step(state):
+    pass
+
+
+def draw(state):
+    pass
+
+
+def on_key_press(state, key):
+    pass
+
+
+start(init, step, draw, on_key_press)
