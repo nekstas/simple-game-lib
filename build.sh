565b01107223481fcbb2bc26e03406c09fa2bdfc
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2025-12-03 22:58:28 +0300

    Change build.sh a little bit

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ hex=$(openssl rand -hex 2)
 mkdir -p build
 cp $1 build/main.py
 cd build
-flet publish main.py --distpath "../r/$2-$hex" --base-url "r/$2-$hex"
+flet publish main.py --distpath "../r/$2-$hex" --base-url "r/$2-$hex" --app-name "$2"
 git add "../r/$2-$hex"
 git commit -m "Add result files for $2-$hex."
 echo "https://s.nekstas.ru/r/$2-$hex"
