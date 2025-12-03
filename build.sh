2e1a76c3e5b614576263f1dc2000af6c40db050c
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2025-12-04 00:05:38 +0300

    Fix build.sh script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ hex=$(openssl rand -hex 2)
 
 mkdir -p build
 cp $1 build/main.py
-cd build
+cd build || exit
 flet publish main.py --distpath "../r/$2-$hex" --base-url "r/$2-$hex" --app-name "$2"
 git add "../r/$2-$hex"
 git commit -m "Add result files for $2-$hex."
